fa298542dc0edc0e37b1b92865330d519d7187f5
Kazuma Arino <hogeika2@gmail.com>
2026-06-14 11:06:57 +0900

    Replace FastFile.kt to shared one.
    Copy https://github.com/karino2/FastFile, a09e1fb.

--- app/src/main/java/io/github/karino2/fastfile/FastFile.kt
+++ app/src/main/java/io/github/karino2/fastfile/FastFile.kt
@@ -5,7 +5,13 @@ import android.content.Context
 import android.database.Cursor
 import android.net.Uri
 import android.provider.DocumentsContract
+import android.webkit.MimeTypeMap
+import java.io.BufferedWriter
+import java.io.FileInputStream
+import java.io.OutputStreamWriter
 import java.util.Date
+import java.util.zip.GZIPInputStream
+import java.util.zip.GZIPOutputStream
 
 // similar to DocumentFile, but store metadata at first query.
 data class FastFile(val uri: Uri, val name: String, val lastModified: Long, val mimeType: String, val size: Long, val resolver: ContentResolver) {
@@ -58,28 +64,6 @@ data class FastFile(val uri: Uri, val name: String, val lastModified: Long, val 
             }
         }
 
-        /*
-           @Nullable
-    private static String queryForString(Context context, Uri self, String column,
-            @Nullable String defaultValue) {
-        final ContentResolver resolver = context.getContentResolver();
-
-        Cursor c = null;
-        try {
-            c = resolver.query(self, new String[]{column}, null, null, null);
-            if (c.moveToFirst() && !c.isNull(0)) {
-                return c.getString(0);
-            } else {
-                return defaultValue;
-            }
-        } catch (Exception e) {
-            Log.w(TAG, "Failed query: " + e);
-            return defaultValue;
-        } finally {
-            closeQuietly(c);
-        }
-    }
-         */
         // Similar to DocumentFile:fromTreeUri.
         // treeUri is Intent#getData() of ACTION_OPEN_DOCUMENT_TREE
         fun fromTreeUri(context: Context, treeUri: Uri) : FastFile {
@@ -113,6 +97,32 @@ data class FastFile(val uri: Uri, val name: String, val lastModified: Long, val 
     val isFile: Boolean
         get() = !(isDirectory || mimeType == "")
 
+    fun readText()  = resolver.openFileDescriptor(uri, "r")!!.use {desc->
+        val fis = FileInputStream(desc.fileDescriptor)
+        fis.bufferedReader().use { it.readText() }
+    }
+
+    fun writeText(content: String) = resolver.openOutputStream(uri, "wt").use {
+        val writer = BufferedWriter(OutputStreamWriter(it))
+        writer.use {
+            writer.write(content)
+        }
+    }
+
+    fun readGZIPText() = resolver.openInputStream(uri)!!.use {
+        val gzis = GZIPInputStream(it)
+        gzis.bufferedReader().use { reader -> reader.readText() }
+    }
+
+    fun writeGZIPText(content: String) = resolver.openOutputStream(uri, "wt").use {
+        val gzos = GZIPOutputStream(it)
+        val writer = BufferedWriter(OutputStreamWriter(gzos))
+        writer.use { w ->
+            w.write(content)
+        }
+    }
+
+
     //
     //  funcs below are for directory only
     //
@@ -126,11 +136,34 @@ data class FastFile(val uri: Uri, val name: String, val lastModified: Long, val 
 
     fun listFiles() =  listFiles(resolver, uri)
 
+
     fun findFile(targetDisplayName: String) = listFiles().find { it.name == targetDisplayName }
+
+    fun findFileRec(relativePath: String) : FastFile? {
+        return relativePath.split("/").fold(this as FastFile?) { acc, disp -> acc?.findFile(disp) }
+    }
+
+    fun nameToMimeType(name: String) : String {
+        val extension = name.substringAfterLast('.', "")
+        return MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension) ?: "application/octet-stream"
+    }
+
+    fun createFileRec(relativePath: String) : FastFile? {
+        val paths =  relativePath.split("/")
+        val last = paths.last()
+        val parent = paths.dropLast(1).fold(this as FastFile) { acc, disp -> acc.findFile(disp) ?: acc.createDirectory(disp)!! }
+        val mimeType = nameToMimeType(last)
+
+        return parent.createFile(mimeType, last)
+    }
 
     fun createDirectory(displayName: String): FastFile? {
         val resUri = DocumentsContract.createDocument(resolver, uri, DocumentsContract.Document.MIME_TYPE_DIR, displayName) ?: return null
         return fromDocUri(resolver, resUri)
+    }
+
+    fun ensureDirectory(displayName: String) : FastFile? {
+        return findFile(displayName) ?: createDirectory(displayName)
     }
 
     val isEmpty : Boolean
